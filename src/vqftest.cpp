e846fa4555af502e63034b9b72f5b2c17c230008
nekomona <nekomona@163.com>
2023-07-07 01:17:46 +0800

    Test for FusionAHRS performance

--- src/vqftest.cpp
+++ src/vqftest.cpp
@@ -1,5 +1,6 @@
 #include <CH58x_common.h>
 #include <vqf.h>
+#include <Fusion.h>
 
 struct BMI160VQFParams: VQFParams {
     BMI160VQFParams() : VQFParams() {
@@ -21,8 +22,37 @@ float fake_acc[3] = {0.54f, 0.72f, 0.0f};
 float fake_mag[3] = {0.3f, 0.4f, 0.1f};
 float quat_out[4];
 
+// Define calibration (replace with actual calibration data if available)
+const FusionMatrix gyroscopeMisalignment = {1.0f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f, 0.0f, 1.0f};
+const FusionVector gyroscopeSensitivity = {1.0f, 1.0f, 1.0f};
+const FusionVector gyroscopeOffset = {0.0f, 0.0f, 0.0f};
+const FusionMatrix accelerometerMisalignment = {1.0f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f, 0.0f, 1.0f};
+const FusionVector accelerometerSensitivity = {1.0f, 1.0f, 1.0f};
+const FusionVector accelerometerOffset = {0.0f, 0.0f, 0.0f};
+const FusionMatrix softIronMatrix = {1.0f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f, 0.0f, 1.0f};
+const FusionVector hardIronOffset = {0.0f, 0.0f, 0.0f};
+
+// Initialise algorithms
+FusionOffset offset;
+FusionAhrs ahrs;
+
 extern "C"
 void vqftest_setup() {
+
+    FusionOffsetInitialise(&offset, 100);
+    FusionAhrsInitialise(&ahrs);
+
+    // Set AHRS algorithm settings
+    const FusionAhrsSettings settings = {
+            .convention = FusionConventionNwu,
+            .gain = 0.5f,
+            .accelerationRejection = 10.0f,
+            .magneticRejection = 20.0f,
+            .rejectionTimeout = 5 * 100, /* 5 seconds */
+    };
+    FusionAhrsSetSettings(&ahrs, &settings);
+
+
     vqf.resetState();
 
     GPIOB_ResetBits(GPIO_Pin_4 | GPIO_Pin_1 | GPIO_Pin_3);
@@ -45,4 +75,33 @@ void vqftest_run() {
     GPIOB_SetBits(GPIO_Pin_2);
     vqf.getQuat9D(quat_out);
     GPIOB_ResetBits(GPIO_Pin_4 | GPIO_Pin_1 | GPIO_Pin_2);
+
+
+
+    FusionVector gyroscope = {0.0f, 0.0f, 0.1f}; // replace this with actual gyroscope data in degrees/s
+    FusionVector accelerometer = {0.54f, 0.72f, 0.0f}; // replace this with actual accelerometer data in g
+    FusionVector magnetometer = {0.3f, 0.4f, 0.1f}; // replace this with actual magnetometer data in arbitrary units
+
+    // Apply calibration
+    gyroscope = FusionCalibrationInertial(gyroscope, gyroscopeMisalignment, gyroscopeSensitivity, gyroscopeOffset);
+    accelerometer = FusionCalibrationInertial(accelerometer, accelerometerMisalignment, accelerometerSensitivity, accelerometerOffset);
+    magnetometer = FusionCalibrationMagnetic(magnetometer, softIronMatrix, hardIronOffset);
+
+    GPIOB_SetBits(GPIO_Pin_2);
+
+    // Update gyroscope offset correction algorithm
+    gyroscope = FusionOffsetUpdate(&offset, gyroscope);
+
+    GPIOB_ResetBits(GPIO_Pin_2);
+
+    // Update gyroscope AHRS algorithm
+    FusionAhrsUpdate(&ahrs, gyroscope, accelerometer, magnetometer, 0.01f);
+
+    GPIOB_SetBits(GPIO_Pin_2);
+
+    // Print algorithm outputs
+    const FusionQuaternion quat = FusionAhrsGetQuaternion(&ahrs);
+    const FusionVector earth = FusionAhrsGetEarthAcceleration(&ahrs);
+    
+    GPIOB_ResetBits(GPIO_Pin_2);
 }
