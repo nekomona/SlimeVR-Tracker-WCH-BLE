5ff59a7dd5f62d6869506777cbe424486aabb4c6
nekomona <nekomona@163.com>
2023-07-07 10:33:57 +0800

    Remove VQF

--- src/vqftest.cpp
+++ src/vqftest.cpp
@@ -1,26 +1,5 @@
 #include <CH58x_common.h>
-#include <vqf.h>
 #include <Fusion.h>
-
-struct BMI160VQFParams: VQFParams {
-    BMI160VQFParams() : VQFParams() {
-        #ifndef VQF_NO_MOTION_BIAS_ESTIMATION
-        motionBiasEstEnabled = false;
-        #endif
-        tauAcc = 2.0f;
-        restMinT = 2.0f;
-        restThGyr = 0.6f; // 400 norm
-        restThAcc = 0.06f; // 100 norm
-    }
-};
-
-BMI160VQFParams vqfParams {};
-VQF vqf(vqfParams, 0.01f, 0.01f, 0.01f);
-
-float fake_gyr[3] = {0.0f, 0.0f, 0.1f};
-float fake_acc[3] = {0.54f, 0.72f, 0.0f};
-float fake_mag[3] = {0.3f, 0.4f, 0.1f};
-float quat_out[4];
 
 // Define calibration (replace with actual calibration data if available)
 FusionMatrix gyroscopeMisalignment = {1.0f, 0.1f, 0.1f, 0.1f, 1.0f, 0.1f, 0.1f, 0.1f, 1.0f};
@@ -40,7 +19,6 @@ extern "C"
 void vqftest_setup() {
 
     FusionOffsetInitialise(&offset, 100);
-    FusionAhrsInitialise(&ahrs);
 
     // Set AHRS algorithm settings
     const FusionAhrsSettings settings = {
@@ -51,9 +29,7 @@ void vqftest_setup() {
             .rejectionTimeout = 5 * 100, /* 5 seconds */
     };
     FusionAhrsSetSettings(&ahrs, &settings);
-
-
-    vqf.resetState();
+    FusionAhrsReset(&ahrs);
 
     GPIOB_ResetBits(GPIO_Pin_4 | GPIO_Pin_1 | GPIO_Pin_3);
     GPIOB_ModeCfg(GPIO_Pin_4 | GPIO_Pin_1 | GPIO_Pin_2, GPIO_ModeOut_PP_20mA);
@@ -62,21 +38,6 @@ void vqftest_setup() {
 extern "C"
 void vqftest_run() {
     GPIOB_SetBits(GPIO_Pin_4 | GPIO_Pin_1);
-    vqf.updateGyr(fake_gyr, 0.01f);
-    GPIOB_SetBits(GPIO_Pin_2);
-    vqf.updateGyr(fake_gyr, 0.01f);
-    vqf.updateGyr(fake_gyr, 0.01f);
-    GPIOB_ResetBits(GPIO_Pin_2);
-    vqf.updateGyr(fake_gyr, 0.01f);
-    GPIOB_SetBits(GPIO_Pin_2);
-    vqf.updateAcc(fake_acc);
-    GPIOB_ResetBits(GPIO_Pin_2);
-    vqf.updateMag(fake_mag);
-    GPIOB_SetBits(GPIO_Pin_2);
-    vqf.getQuat9D(quat_out);
-    GPIOB_ResetBits(GPIO_Pin_4 | GPIO_Pin_1 | GPIO_Pin_2);
-
-
 
     FusionVector gyroscope = {0.0f, 0.0f, 0.1f}; // replace this with actual gyroscope data in degrees/s
     FusionVector accelerometer = {0.54f, 0.72f, 0.0f}; // replace this with actual accelerometer data in g
@@ -103,5 +64,5 @@ void vqftest_run() {
     const FusionQuaternion quat = FusionAhrsGetQuaternion(&ahrs);
     const FusionVector earth = FusionAhrsGetEarthAcceleration(&ahrs);
     
-    GPIOB_ResetBits(GPIO_Pin_2);
+    GPIOB_ResetBits(GPIO_Pin_4 | GPIO_Pin_1 | GPIO_Pin_2);
 }
